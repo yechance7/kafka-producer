7d526151e192d69a28dc9e97129cd0f790820995
Yechan <yechance7@gmail.com>
2025-12-21 17:57:05 +0900

    fix: intermediate save for troubleshooting

--- deploy/after_install.sh
+++
@@ -1,3 +0,0 @@
-source /src/kafka_venv/bin/activate
-python3 /src/kafka-producer/deploy/replace_secret.py
-pip3 install -r /src/kafka-producer/requirements.txt
--- deploy/application_start.sh
+++
@@ -1,4 +0,0 @@
-if [ "$HOSTNAME" == "kafka01" ]; then
-  systemctl stop bicycle-producer.service
-  systemctl start bicycle-producer.service
-fi
